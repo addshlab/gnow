b92cba127024f5be6949e64ebdb638b1a7eca4a9
nakad <nakad@add.sh>
2021-09-05 04:36:29 +0900

    Add shell color

--- gnow/color.py
+++ gnow/color.py
@@ -1,17 +1,25 @@
 class ColorClass:
   def __init__(self):
     self.colors = {
+      'gray'   : '\033[30',
       'red'    : '\033[31',
       'green'  : '\033[32',
       'yellow' : '\033[33',
+      'blue'   : '\033[34',
+      'purple' : '\033[35',
+      'lime'   : '\033[36',
       'white'  : '\033[37',
     }
 
     self.backgrounds = {
-      'white'  : ';47',
-      'red'    : ';41',
-      'green'  : ';42',
-      'yellow' : ';43',
+        'gray'   : ';40',
+        'red'    : ';41',
+        'green'  : ';42',
+        'yellow' : ';43',
+        'blue'   : ';44',
+        'purple' : ';45',
+        'lime'   : ';46',
+        'white'  : ';47',
     }
 
     self.end    = '\033[0m'
